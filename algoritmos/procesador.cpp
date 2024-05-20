6b639495f09e73754f9a92227a3fa5b2d9cd12fa
Jorge Ktch <jorgektch@gmail.com>
2024-05-19 22:24:54 -0500

    Crear fichero

---
+++ algoritmos/procesador.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<string> textos = {
+        "Un pollo broaster parte pecho puerta 3",
+        "Ají de gallina puerta 7",
+    };
+    
+    return 0;
+}
